b2536e0f50e7def8b10cb56eace913d38abd7f74
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-28 17:05:10 -0700

    second

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 def main():
     plist = []
-    ##################################################
-    # Comlete your code here
-    ##################################################
+
+    begin = int(input('Enter a number greater than 1: '))
+    end = int(input('Enter a number greater than begin: '))
+    
+    if begin > 1 and begin < end:
+        for x in range(begin, end+1):
+            is_prime = True
+            if x <=1:
+                is_prime = False
+            for i in range (2, int(x**.5) + 1):
+                if x % i == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                plist.append(x)
+    print(*plist)
+
 
     return plist
 
